a8efa7f9072ddaabc812293b916670dfebde06df
unknown <buiduythanh24399@gmail.com>
2021-03-29 10:57:40 +0700

    Update frontend

--- static/js/employee.js
+++ static/js/employee.js
@@ -1,3 +1,17 @@
-app.controller('employeeCtrl', function($scope) {
-    $scope.msg = "Employee";
+app.controller('employeeCtrl', function ($scope, filterFilter) {
+    $scope.sortType = 'name';
+    $scope.sortReverse = false;
+    $scope.checkPosition = checkAllScope;
+    $scope.employee = [
+        { name: 'Nguyen', age: 20, nickname: "Line", position: "A" },
+        { name: 'Linh', age: 18, nickname: "Linh", position: "CB" },
+        { name: 'Hoa', age: 15, nickname: "Hoa", position: "B" },
+        { name: 'Nga', age: 35, nickname: "Nga", position: "M" },
+        { name: 'Zoan', age: 26, nickname: "Zoan", position: "Y" },
+    ];
+    // $scope.success = false;
+    //         $scope.register = function () {
+    //            $scope.success = true;
+    //         }
 });
+
--- static/js/position.js
+++ static/js/position.js
@@ -1,3 +1,26 @@
-app.controller('positionCtrl', function($scope) {
-    $scope.msg = "Position";
+app.controller('positionCtrl', function ($scope, filterFilter) {
+    $scope.sortType = 'name';
+    $scope.sortReverse = false;
+    $scope.checkPosition = checkAllScope;
+    $scope.students = [
+        { name: 'Lien', age: 20, nickname: "Line", position: "A" },
+        { name: 'Linh', age: 18, nickname: "Linh", position: "CB" },
+        { name: 'Hoa', age: 15, nickname: "Hoa", position: "B" },
+        { name: 'Nga', age: 35, nickname: "Nga", position: "M" },
+        { name: 'Zoan', age: 26, nickname: "Zoan", position: "Y" },
+    ];
+    $scope.position = [
+        {
+            id: 1,
+            names: "CEO"
+        },
+        {
+            id: 2,
+            names: "Manager"
+        },
+        {
+            id: 3,
+            names: "Admin"
+        }
+    ]
 });
