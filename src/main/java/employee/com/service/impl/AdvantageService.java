681470a6fe5af0e513dd748f94b280803b46eb1b
nguyenhoduyquy <59812329+nguyenhoduyquy@users.noreply.github.com>
2021-04-07 15:00:53 +0700

    Add files via upload

---
+++ src/main/java/employee/com/service/impl/AdvantageService.java
@@ -0,0 +1,66 @@
+ package employee.com.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import employee.com.DTO.AdvantageDTO;
+import employee.com.entity.AdvantageEntity;
+import employee.com.repository.AdvantageRepository;
+import employee.com.repository.UserRepository;
+import employee.com.service.IAdvantageService;
+
+@Service
+public class AdvantageService implements IAdvantageService {
+	
+	@Autowired
+	private AdvantageRepository advantageRepository;
+	
+//	@Autowired
+//	private UserRepository userRepository;
+	
+	@Override
+	public List<AdvantageDTO> findAll(Long id) {
+		List<AdvantageDTO> result = new ArrayList<>();
+		List<AdvantageEntity> entitys = advantageRepository.findAll();
+		for (AdvantageEntity advantageEntity : entitys) {
+			AdvantageDTO dto = new AdvantageDTO();
+			dto.setId(advantageEntity.getId());
+			dto.setName(advantageEntity.getName());
+			result.add(dto);
+			}
+		return result;
+	}
+
+	@Override
+	public void saveAdvantage(AdvantageDTO dto) {
+		AdvantageEntity advantageEntity = new AdvantageEntity();
+		advantageEntity.setName(dto.getName());
+		advantageRepository.save(advantageEntity);
+	}
+	
+//	@Override
+//	public void deleteAdvantage(Long id) {
+//		AdvantageEntity advantageEntity = new AdvantageEntity();
+//		advantageEntity.setId(id);
+//		advantageRepository.delete(advantageEntity);
+//	}
+
+	@Override
+	public void deleteListAdvantage(Long[] ids) {
+		for(Long id : ids) {
+			advantageRepository.delete(id);
+		}
+	}
+	
+	@Override
+	public void updateAdvantage(AdvantageDTO dto, Long id) {
+		AdvantageEntity advantageEntity = advantageRepository.getOne(id);
+		advantageEntity.setName(dto.getName());
+		advantageRepository.save(advantageEntity);
+	}
+	
+	
+}
